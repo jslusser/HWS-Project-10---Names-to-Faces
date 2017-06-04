89f350916bffc20032651e58cbb10970634d7003
jslusser <jslusser@gmail.com>
2017-06-04 12:14:40 +0400

    Design UICollectionView cells

---
+++ Project10/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by James Slusser on 6/4/17.
+//  Copyright © 2017 James Slusser. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+}
